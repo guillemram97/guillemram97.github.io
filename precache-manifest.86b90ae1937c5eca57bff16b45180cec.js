b1f0371dcd422d74cf982dcef79d6065f64dea70
Guillem Ramírez <guillemram97@gmail.com>
2021-05-25 19:47:27 +0200

    Updates

---
+++ precache-manifest.86b90ae1937c5eca57bff16b45180cec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c82651957731eaac42ba4995bb3c2bb",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d324d42fe5ac4e540e7d",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "d6668b131ec679828d22",
+    "url": "/home/static/js/2.41ccced1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.41ccced1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324d42fe5ac4e540e7d",
+    "url": "/home/static/js/main.2d6e285f.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "79f200608765025b3816142764c94de3",
+    "url": "/home/static/media/aicoliseum.79f20060.png"
+  },
+  {
+    "revision": "2c3aa38d30c3b4ad6fe58180b9b7b3cb",
+    "url": "/home/static/media/audiochunks.2c3aa38d.png"
+  },
+  {
+    "revision": "62240d82c8c305373d9be0175ace5378",
+    "url": "/home/static/media/epicgame.62240d82.png"
+  },
+  {
+    "revision": "6c7b732114962f75961026b8a19d1f5d",
+    "url": "/home/static/media/guillemrams.6c7b7321.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "97264028e8c7e0bae635a384d92f325c",
+    "url": "/home/static/media/resume.97264028.pdf"
+  }
+]);
